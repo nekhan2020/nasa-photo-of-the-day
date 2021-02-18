f9e273a7c239fb5af3bbe85fef3fe2942466aa94
Nadeem Khan <nadeemekhan@gmail.com>
2021-02-18 15:48:15 -0800

    styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Nasapost from "./components/NasaPost";
 import NavBar from "./components/NavBar";
 import axios from "axios";
 import { BASE_URL, API_KEY } from "./resources/apiInfo";
+import { ThemeProvider } from "styled-components";
+import theme from "./components/theme";
+import Style from "styled-components";
 
 function App() {
   const [media, setMedia] = useState();
@@ -13,32 +16,42 @@ function App() {
   const [link, setLink] = useState();
   const [author, setAuthor] = useState();
 
-  axios
-    // .get("#")
-    .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
-    .then((res) => {
-      setMedia(res.data.media_type);
-      setDate(res.data.date);
-      setTitle(res.data.title);
-      setExplanation(res.data.explanation);
-      setLink(res.data.url);
-      setAuthor(res.data.copyright);
-    })
-    .catch((error) => {
-      console.error(`this is the error coming from axios: ${error} `);
-    });
+  const StyledBody = Style.div`
+  background: ${(props) => props.theme.backgroundColor}
+
+`;
+  useEffect(() => {
+    axios
+      // .get("#")
+      .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
+      .then((res) => {
+        setMedia(res.data.media_type);
+        setDate(res.data.date);
+        setTitle(res.data.title);
+        setExplanation(res.data.explanation);
+        setLink(res.data.url);
+        setAuthor(res.data.copyright);
+      })
+      .catch((error) => {
+        console.error(`this is the error coming from axios: ${error} `);
+      });
+  }, []);
   return (
-    <div className="App">
-      <NavBar></NavBar>
-      <Nasapost
-        date={date}
-        title={title}
-        media={media}
-        explanation={explanation}
-        author={author}
-        link={link}
-      ></Nasapost>
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledBody>
+        <div className="App">
+          <NavBar></NavBar>
+          <Nasapost
+            date={date}
+            title={title}
+            media={media}
+            explanation={explanation}
+            author={author}
+            link={link}
+          ></Nasapost>
+        </div>
+      </StyledBody>
+    </ThemeProvider>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,13 @@ import nasaLogo from "../assets/nasaLogo.jpg";
 
 const NavBar = (props) => {
   return (
-    <div>
-      <div>{/* <img src={nasaLogo} alt="" /> */}</div>
-      <h1>Nasa Photo of the Day</h1>
-      <img src={nasaLogo} alt="" className="nasaLogo" />
+    <div className="navParent">
+      <div className="navLogoDiv">
+        <img src={nasaLogo} alt="" className="nasaLogo" />
+      </div>
+      <div className="navH1Div">
+        <h3>Nasa Photo of the Day</h3>
+      </div>
     </div>
   );
 };
